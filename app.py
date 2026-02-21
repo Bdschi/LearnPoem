554cda5732e3425089c7739b68f376c65f637140
bdschi <kadmus@nexgo.de>
2026-02-21 17:04:11 +0100

    Special treatmemt for versions of alif

--- app.py
+++ app.py
@@ -94,9 +94,15 @@ def current_user():
 
 # ── Text comparison helpers ────────────────────────────────────────────────────
 def normalize_arabic_text(text):
-    # Step 1: Replace Alef Wasla (ٱ) with regular Alif (ا)
+    # Unicode for superscript alif
+    superscript_alif = '\u0670'
+    # Step 1: Replace superscript alif with regular alif
+    text = text.replace(superscript_alif, 'ا')
+    # Replace U+0640 "ARABIC Tatweel" ("ـ").
+    text = text.replace('\u0640', '')
+    # Step 2: Replace Alef Wasla (ٱ) with regular Alif (ا)
     text = text.replace('\u0671', '\u0627')
-    # Step 2: Remove Arabic diacritics (U+064B to U+0655)
+    # Step 3: Remove Arabic diacritics (U+064B to U+0655)
     arabic_diacritics = re.compile(r'[\u064B-\u0655]')
     text = arabic_diacritics.sub('', text)
     return text
